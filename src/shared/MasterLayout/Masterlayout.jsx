07c71c4db303c100bff9d684a35b854afcb23b83
MohamedMatter2000 <mahmouedmatter@yahoo.com>
2025-05-20 22:11:25 +0300

    Fix

--- src/shared/MasterLayout/Masterlayout.jsx
+++ src/shared/MasterLayout/Masterlayout.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
-import MainSidebar from "../Sidebar/MainSidebar";
+import MainSidebar from "../SIdebar/MainSidebar";
 export default function Masterlayout() {
   return (
     <div
