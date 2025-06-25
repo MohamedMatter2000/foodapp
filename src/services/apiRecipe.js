af53dc8dcc146991ed64d6d5d83eb711623e6d45
MohamedMatter2000 <mahmouedmatter@yahoo.com>
2025-06-25 13:22:04 +0300

    fixerrordeploy

--- src/services/apiRecipe.js
+++ src/services/apiRecipe.js
@@ -1,6 +1,6 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-toastify";
-import { apiClient } from "./APiconfig";
+import { apiClient } from "./aPiConfig";
 export const useRecipes = (params = {}) => {
   const { data, isPending } = useQuery({
     queryKey: ["Recipes", params],
